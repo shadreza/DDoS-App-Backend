e9508a882f2bafb380afae5ad61c2c7e52e7f4fb
shadreza <shadreza100@gmail.com>
2023-02-05 22:18:04 +0600

    The fastApi is now doing get and post

---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "https://ddos-app-shadreza.vercel.app",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class Data(BaseModel):
+    data: object
+
+@app.get("/")
+async def root():
+  return {"data" : "السلام عليكم ورحمة الله وبركاته"}
+
+@app.post("/data")
+async def sendDataToBE(data: Data):
+  return data
