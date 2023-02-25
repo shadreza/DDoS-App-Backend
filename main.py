e5442d662e663c27c317a43115c83970c676a462
shadreza <shadreza100@gmail.com>
2023-02-25 22:40:15 +0600

    trying the final fetch

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import pickle
 import joblib
 import numpy as np
 import pandas as pd
-import tensorflow as tf
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
