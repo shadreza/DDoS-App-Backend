adf431f6e871a93a3288a612d5153484948a6a6d
shadreza <shadreza100@gmail.com>
2023-02-26 00:14:18 +0600

    Alhamdulillah

--- main.py
+++ main.py
@@ -10,10 +10,7 @@ from pydantic import BaseModel
 app = FastAPI()
 
 
-pickleFile = './Decision_Tree_model_on_RENN.pkl'
 
-pickle_in = open(pickleFile, 'rb')
-classifiers = pickle.load(pickle_in)
 
 origins = [
     "http://localhost:3000",
@@ -31,8 +28,8 @@ app.add_middleware(
 )
 
 
-model = joblib.load(pickleFile)
 pickleFile = './Decision_Tree_model_on_RENN.pkl'
+model = joblib.load(pickleFile)
 
 impFeatureSet = [
     "ACK Flag Count",
@@ -79,73 +76,66 @@ impFeatureSet = [
     "URG Flag Count"
   ]
 
+def formatString(strValue):
+  dotPos = strValue.find('.')
+  s = ""
+  typeOfString = 1
+  if dotPos == -1:
+    # int
+    typeOfString = 1
+  else:
+    # float
+    typeOfString = 2
+  for i  in strValue:
+    if(i >= '0' and i <= '9'):
+      s+=i
+  if typeOfString == 1:
+    return int(s)
+  elif typeOfString == 2:
+    return float(s)
+  return 0
+
+
 class Data(BaseModel):
-    data: object
+    data: list = []
 
 @app.get("/")
 async def root():
   return {"data" : "السلام عليكم ورحمة الله وبركاته"}
 
 @app.post("/data")
-
 async def sendDataToBE(data: Data):
 
-  P = []
-  p = []
+  name = "Idle Min\r"
 
-  for feature in impFeatureSet:
-    if(feature == "Idle Min"):
-      p.append(0.0)
-    else:
-      p.append(float(data.data[feature]))
-  P.append(p)
+  try:
+    data.data[0]["Idle Min\r"]
+    name = "Idle Min\r"
+  except:
+    try:
+      data.data[0]["Idle Min"]
+      name = "Idle Min"
+    except:
+      name = ""
+
+  P = []
+
+  for row in data.data:
+    p = []
+    for feature in impFeatureSet:
+      if(feature == "Idle Min"):
+        if name == "":
+          p.append(0.0)
+        else:
+          p.append(formatString(row[feature]))
+      else:
+        p.append(formatString(row[feature]))
+    
+    P.append(p)
 
   df = pd.DataFrame(P, columns=impFeatureSet)
-
   res = (model.predict(df))
-  return res
-
-# @app.post("/predict")
-
-# async def predictData(data: Data):
-#   P = []
-#   p = []
-
-#   pickleFiles = ['./Models/Central_Trained_Models/Bi-GRUModel.pkl', './Models/Central_Trained_Models/LSTMModel.pkl', './Models/Central_Trained_Models/GRUModel.pkl', './Models/Central_Trained_Models/Bi-LSTMModel.pkl', ]
-
-
-#   model1C = joblib.load(pickleFiles[0])
-#   model2C = joblib.load(pickleFiles[1])
-#   model3C = joblib.load(pickleFiles[2])
-#   model4C = joblib.load(pickleFiles[3])
-
-
-
-#   for feature in impFeatureSet:
-#     if(feature == "Idle Min"):
-#       p.append(0.0)
-#     else:
-#       p.append(float(data.data[feature]))
-#   P.append(p)
-#   print(P)
-
-#   X = pd.DataFrame(P, columns=impFeatureSet)
-
-
-#   #Making predictions
-#   models = [model1C, model2C, model3C, model4C]
-
-#   preds = [model.predict(X) for model in models]
-#   preds=np.array(preds)
-
-
-#   summed = np.sum(preds, axis=0)
-
-#   # # argmax across classes
-#   ensemble_prediction = np.argmax(summed, axis=1)
-
-
-#   counts = np.bincount(ensemble_prediction)
-#   max_count_class = np.argmax(counts)
-#   return(max_count_class)
-
+  resultArray = []
+  for i in res:
+    resultArray.append(i)
+  return resultArray
